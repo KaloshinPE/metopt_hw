gradient decend works, time to fix output

logistic_regression/main.py
@@ -3,17 +3,17 @@ from matplotlib import pyplot as plt
 import random
 
 Number_of_points = 1000
-acc = 0.01 # accurasy
+acc = 0.001 # accurasy
 
 
 first_average_x = -10.0
 first_dispersion_x = 7.0
-first_average_y = -10.0
+first_average_y = 10.0
 first_dispersion_y = 7.0
 
 second_average_x = 10.0
 second_dispersion_x = 8.0
-second_average_y = 10.0
+second_average_y = -10.0
 second_dispersion_y = 8.0
 
 # generating data randomly
@@ -74,7 +74,7 @@ def grad_Q(w, Xo):
 def make_step(Xo, w, grad_Xo, grad_w):
     # calculates step via optimizing one-dimensional function F(t) = Q(Xo|w + t*gradQ(Xo, w))
     # returns new Xo, w
-    min_step = acc/(np.linalg.norm(grad_Xo) + np.linalg.norm(grad_w))
+    min_step = acc/np.sqrt(np.linalg.norm(grad_Xo)**2 + np.linalg.norm(grad_w)**2)
     t1 = 0
     t2 = min_step
     Q1, Q2 = Q(w, Xo), 0
@@ -85,7 +85,8 @@ def make_step(Xo, w, grad_Xo, grad_w):
             t1 = t2
             t2 *= 2
         else: break
-    while(True):
+
+    while True:
         if(t2-t1 <= min_step):
             break
         else:
@@ -97,8 +98,8 @@ def make_step(Xo, w, grad_Xo, grad_w):
             else:
                 t2 = t
                 Q2 = Q_mid
-    if t1 < acc:
-        return np.zeros(2), np.zeros(2)
+    if t1*np.sqrt(np.linalg.norm(grad_Xo)**2 + np.linalg.norm(grad_w)**2) < acc:
+        return Xo, w
     else:
         return Xo - t1*grad_Xo, w - t1*grad_w
 
@@ -114,12 +115,15 @@ def draw_line(Xo, w, color):
     Y_bottom = second_average_y - scale*second_dispersion_y
     if(w[0] != 0 and w[1] != 0):
         direction = [np.abs(w[1]/w[0]), 1.0]
-
-        if((X_right - Xo[0])/direction[0] > (Y_top - Xo[1])/direction[1]):
+        if w[1]/w[0] < 0:
+            f = X_left
+            X_left = X_right
+            X_right = f
+        if(np.abs(X_right - Xo[0])/direction[0] > (Y_top - Xo[1])/direction[1]):
             X_finish = Xo[0] + (Y_top - Xo[1])/direction[1]*direction[0]
         else: X_finish = X_right
 
-        if ((Xo[0]-X_left) / direction[0] > (Xo[1]-Y_bottom) / direction[1]):
+        if (np.abs(Xo[0]-X_left) / direction[0] > (Xo[1]-Y_bottom) / direction[1]):
             X_start = Xo[0] - (Xo[1]-Y_bottom) / direction[1] * direction[0]
         else:
             X_start = X_left
@@ -128,8 +132,8 @@ def draw_line(Xo, w, color):
         X_start = X_left
         X_finish = X_right
     elif w[1] == 0:
-        X_start = X_left
-        X_finish = X_right
+        X_start = Xo[0]
+        X_finish = Xo[0]
 
     X = np.array([X_start, X_finish])
     if(w[1] != 0):
@@ -143,21 +147,22 @@ def draw_line(Xo, w, color):
 # Xo - point in the hyperplane, W - normal vector to hyperplane
 # Xo = np.array([(first_average_x + second_average_x)/2.0, (first_average_y + second_average_y)/2.0])
 # w =  np.array([(second_average_x - first_average_x), (second_average_y - first_average_y)])
-Xo = np.array([3.0, 5.0])
-w = np.array([10.0, 0.0])
+Xo = np.array([10.0, -5.0])
+w = np.array([1.0, 0.0])
 draw_line(Xo, w, 'g')
 
 # gradient decent
 while(True):
-    print "cock"
     grad_Xo, grad_w = grad_Q(w, Xo)
     X1, w1 = make_step(Xo, w, grad_Xo, grad_w)
+    print grad_Xo, grad_w
     if np.linalg.norm(X1-Xo)+ np.linalg.norm(w1 - w) <= acc:
         break
     else:
         Xo, w = X1, w1
 
 # plotting final hyperplane
+print Xo, w
 draw_line(Xo, w, 'm')
 
 plt.show()