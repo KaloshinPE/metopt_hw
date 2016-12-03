ab982cba5378655bb1ba69d67f752eabefd80965
Pavel <pavelkaloshin@gmail.com>
2016-12-03 13:02:49 +0300

    reconstracted vector w

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -17,7 +17,7 @@ second_dispersion_y = 8
 
 random.seed()
 for i in range(Number_of_points):
-    point = []
+    point = list()
     point.append(random.randrange(-1, 2, 2))
     if point[0] == 1:
         point.append(random.gauss(first_average_x, first_dispersion_x))
@@ -35,13 +35,21 @@ x = [points[i][1] for i in range(len(points)) if points[i][0] == -1]
 y = [points[i][2] for i in range(len(points)) if points[i][0] == -1]
 plt.plot(x, y, 'bo')
 
-# flat
-w = [(first_average_x + second_average_x)/2.0*(second_average_x - first_average_x) +
-     (first_average_y + second_average_y)/2.0 *(second_average_y - first_average_y),
+
+def M(X, w):
+    # calculating yi*<xi,w>
+    return X[0]*(X[1]*w[1] + X[2]*w[2] - w[0])
+
+# start hyperplane
+
+# w is 2*N dimensional vector, consisted from two vectors - Xo (point in the hyperplane)
+# and W (normal vector to hyperplane)
+w = [(first_average_x + second_average_x)/2.0,
+     (first_average_y + second_average_y)/2.0 ,
      (second_average_x - first_average_x), (second_average_y - first_average_y)]
 X = np.linspace(first_average_x - 0.5*first_dispersion_x, second_average_x + 0.5*second_dispersion_x, 2)
-Y = (w[0] - X*w[1])/float(w[2])
+Y = (w[0]*w[2] + w[1]*w[3] - X*w[2])/float(w[3])
 lines = plt.plot(X, Y)
-plt.setp(lines, color = 'g', linewidth = 3.0)
+plt.setp(lines, color='g', linewidth=3.0)
 
 plt.show()
