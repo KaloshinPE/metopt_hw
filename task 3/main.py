e25d35ff896015e621b8a322f6f0e96a487f4912
Pavel <pavelkaloshin@gmail.com>
2016-12-06 22:54:44 +0300

    simplex method implemented, but there is a mistake somewhere

--- task 3/main.py
+++ task 3/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import decimal as dc
+from scipy.optimize import linprog
 
 # distortion
 sigma = 0.2
@@ -49,7 +49,37 @@ a_est2 = np.linalg.solve(A, b)
 y_est2 = values(a_est2)
 
 
+# minimize sum of modules using simplex algorithm
+A = np.zeros([2*int(m), int(m) + 6])
+c = np.ones(int(m) + 6)
+b = np.zeros(2*int(m))
+for i in range(6):
+    c[i] = 0
+for i in range(int(m)):
+    b[2 * i] = -1*y_random[i]
+    b[2 * i + 1] = y_random[i]
+
+    A[2 * i][0] = -1*np.sin(t[i])
+    A[2 * i][1] = np.sin(t[i])
+    A[2 * i][2] = -1 * t[i]
+    A[2 * i][3] = t[i]
+    A[2 * i][4] = -1
+    A[2 * i][5] = 1
+    A[2 * i][i + 6] = -1
+
+    A[2 * i + 1][0] = np.sin(t[i])
+    A[2 * i + 1][1] = -1 * np.sin(t[i])
+    A[2 * i + 1][2] = t[i]
+    A[2 * i + 1][3] = -1 * t[i]
+    A[2 * i + 1][4] = 1
+    A[2 * i + 1][5] = -1
+    A[2 * i + 1][i + 6] = -1
+optimized = linprog(c, A, b)
+print "coeff: " + str([optimized.slack[0] - optimized.slack[1], optimized.slack[2] - optimized.slack[3], optimized.slack[4] - optimized.slack[5]])
+a_est1 = np.array([optimized.slack[0] - optimized.slack[1], optimized.slack[2] - optimized.slack[3], optimized.slack[4] - optimized.slack[5]])
+y_est1 = values(a_est1)
+
 plt.plot(t, y_original, 'r')
-plt.plot(t, y_est2, 'b')
+plt.plot(t, y_est1, 'b')
 plt.plot(t, y_random, 'g')
 plt.show()
