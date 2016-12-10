32ae7117dbdaa2ebbf33164de0642bcb13343795
Pavel <pavelkaloshin@gmail.com>
2016-12-10 19:27:29 +0300

    newton method works, central path works too

--- task2/main.py
+++ task2/main.py
@@ -5,9 +5,9 @@ A = np.array([[1, 2]])
 b = np.array([2])
 c = np.array([3, 4])
 m, n = A.shape[0], A.shape[1]
-acc = 10**(-10)
+acc = 10**(-3)
 e1, e2, e3, e4 = acc, acc, acc, acc
-k_max = 100
+k_max = 300
 
 
 def G_value(x, x_s, y, y_s, mu):
@@ -23,10 +23,12 @@ def grad_G(x, x_s, y, y_s, mu):
     grad_x, grad_x_s, grad_y, grad_y_s = np.zeros(len(x)), np.zeros(len(x_s)), np.zeros(len(y)), np.zeros(len(y_s))
     for i in range(len(x)):
         grad_x[i] = 2*np.dot(r1, A.transpose()[i]) + 2*(x[i]*y_s[i] - mu)*y_s[i]
-        grad_y_s[i] = -2*dot2 + 2*(x[i]*y_s[i] - mu)*x[i]
+        #grad_y_s[i] = -2*dot2 + 2*(x[i]*y_s[i] - mu)*x[i]
+        grad_y_s[i] = -2 * r2[i] + 2 * (x[i] * y_s[i] - mu) * x[i]
     for j in range(len(y)):
         grad_y[j] = 2*np.dot(r2, A[j]) + 2*(x_s[j]*y[j] - mu)*x_s[j]
-        grad_x_s[j] = 2*dot1 + 2*(x_s[j]*y[j] - mu)*y[j]
+        grad_x_s[j] = 2 * r1[j] + 2 * (x_s[j] * y[j] - mu) * y[j]
+        #grad_x_s[j] = 2*dot1 + 2*(x_s[j]*y[j] - mu)*y[j]
     return grad_x, grad_x_s, grad_y, grad_y_s
 
 
@@ -35,22 +37,39 @@ def calc_next_point(x, x_s, y, y_s, mu):
     X = np.hstack((x, x_s, y, y_s))
     gradG_X = np.hstack((grad_x, grad_x_s, grad_y, grad_y_s))
     G_X = G_value(x, x_s, y, y_s, mu)
+    for i in range(len(X)):
+        if gradG_X[i] > 0 and X[i] == 0:
+            gradG_X[i] = 0
     alpha = G_X/np.linalg.norm(gradG_X)
     for i in range(len(X)):
+        if gradG_X[i] > 0 and X[i] == 0:
+            gradG_X[i] = 0
         if gradG_X[i] > 0:
-            alpha = min(alpha, X[i]/gradG_X[i])
-    X_next = X - alpha*gradG_X/np.linalg.norm(gradG_X)
-    print "G: " + str(G_X)
+            alpha = min(alpha, X[i]/(gradG_X[i]/np.linalg.norm(gradG_X)))
+    X_next = X - G_X*gradG_X/np.linalg.norm(gradG_X)**2
+    for i in range(len(X_next)):
+        if X_next[i] < 0:
+            X_next[i] = 0
     return X_next[:len(x)], X_next[len(x):len(x) + len(x_s)], X_next[len(x) + len(x_s):len(x) + len(x_s) + len(y)], X_next[len(x) + len(x_s) + len(y):len(x) + len(x_s) + len(y) + len(y_s)]
 
 
 def solve_G(mu):
     x, y_s, x_s, y = 2*np.ones(n), np.ones(n), np.ones(m), np.ones(m)
-    for i in range(1000):
+    while True:
         x_next, x_s_next, y_next, y_s_next = calc_next_point(x, x_s, y, y_s, mu)
         if np.linalg.norm(x - x_next) <= e1 and np.linalg.norm(x_s - x_s_next) <= e2 and np.linalg.norm(y - y_next) <= e3 and np.linalg.norm(y_s - y_s_next) <= e4:
-                break;
+                break
         x, x_s, y, y_s = x_next, x_s_next, y_next, y_s_next
     return x, x_s, y, y_s
 
-print solve_G(0)
+
+x, y_s, x_s, y = 2*np.ones(n), np.ones(n), np.ones(m), np.ones(m)
+mu = 5
+for i in range(k_max):
+    x_next, x_s_next, y_next, y_s_next = solve_G(mu)
+    if np.linalg.norm(x - x_next) <= e1 and np.linalg.norm(x_s - x_s_next) <= e2 and np.linalg.norm(y - y_next) <= e3 and np.linalg.norm(y_s - y_s_next) <= e4:
+        break;
+    x, x_s, y, y_s = x_next, x_s_next, y_next, y_s_next
+    mu = mu/2
+
+print x, x_s, y, y_s
