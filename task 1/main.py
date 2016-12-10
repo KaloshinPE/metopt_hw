162b09f6f6293415bcd6b8abfa1586e1c86da693
Pavel <pavelkaloshin@gmail.com>
2016-12-10 21:53:08 +0300

    working variant

--- task 1/main.py
+++ task 1/main.py
@@ -1,165 +1,151 @@
+
 import numpy as np
-from matplotlib import pyplot as plt
-import random
+import numpy.random as rand
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import pylab as pb
+from scipy.optimize import minimize
+import math
 
-Number_of_points = 1000
-acc = 0.005 # accurasy
+
+class LogisticRegression:
+    def __init__(self):
+        self.number = 0
+
+    def fit(self, train_data):
+        self.row_number = train_data.shape[0]
+        self.column_number = train_data.shape[1]
+        self.train = np.hsplit(train_data, (0, self.column_number-1))[1]
+        self.target = np.hsplit(train_data, (0, self.column_number-1))[2]
+
+    def Q(self, w):
+            s = 0
+            for i in range(self.row_number):
+                power = -1*self.target[i][0]*np.dot(w, self.train[i][:])
+                s += math.log((1+math.exp(power)), math.e)
+            return s
+
+    def find_weights(self, initial_weights):
+        return gradient_descent(self.Q, initial_weights)
 
 
-first_average_x = 0.0
-first_dispersion_x = 2.0
-first_average_y = 10.0
-first_dispersion_y = 5.0
-
-second_average_x = 0.0
-second_dispersion_x = 2.0
-second_average_y = -10.0
-second_dispersion_y = 2.0
-
-# generating data randomly
-points = []
-random.seed()
-
-for i in range(Number_of_points):
-    point = list()
-    point.append(random.randrange(-1, 2, 2))
-    point.append((random.random()-0.5)*20)
-    point.append((random.random() - 0.5) * 20)
-    '''
-    if point[0] == 1:
-        point.append(random.gauss(first_average_x, first_dispersion_x))
-        point.append(random.gauss(first_average_y, first_dispersion_y))
+def gen_data1(e_value1, e_value2, disp1, disp2, number1, number2, fig):
+    class1 = np.zeros(number1) - 1
+    last_column1 = np.zeros(number1) + 1
+    class2 = np.zeros(number2) - 1
+    last_column2 = np.zeros(number2) - 1
+    for i in e_value1:
+        column = rand.normal(i, disp1, number1)
+        class1 = np.column_stack((class1, column))
+    class1 = np.column_stack((class1, last_column1))
+    for i in e_value2:
+        column = rand.normal(i, disp2, number2)
+        class2 = np.column_stack((class2, column))
+    class2 = np.column_stack((class2, last_column2))
+    x1 = class1[:, 1]
+    y1 = class1[:, 2]
+    x2 = class2[:, 1]
+    y2 = class2[:, 2]
+    if class1.shape[1] > 4:
+        z1 = class1[:, 3]
+        z2 = class2[:, 3]
+        axes = Axes3D(fig)
+        axes = fig.gca(projection='3d')
+        axes.plot(x1, y1, z1, lw = 0, marker = '.', ms = 10, markevery = None, color = 'red')
+        axes.plot(x2, y2, z2, lw = 0, marker = '.', ms = 10, markevery = None, color = 'green')
+        return np.vstack((class1, class2)), axes
     else:
-        point.append(random.gauss(second_average_x, second_dispersion_x))
-        point.append(random.gauss(second_average_y, second_dispersion_y)) '''
-    points.append(point)
-
-x = [points[i][1] for i in range(len(points)) if points[i][0] == -1]
-y = [points[i][2] for i in range(len(points)) if points[i][0] == -1]
-plt.plot(x, y, 'bo')
-
-x = [points[i][1] for i in range(len(points)) if points[i][0] == 1]
-y = [points[i][2] for i in range(len(points)) if points[i][0] == 1]
-plt.plot(x, y, 'ro')
-
-def norm(x):
-    # calculates euclidean norm of vector
-    return np.sqrt(reduce(lambda a, b: a**2 + b**2, x))
+        plt.plot(x1, y1, 'bo')
+        plt.plot(x2, y2, 'ro')
+        return np.vstack((class1, class2)), 0
 
 
-def M(X, w, Xo):
-    # calculates yi*<xi,w>
-    return X[0]*(np.dot(np.array(X[1:]), w) - np.dot(w, Xo))
+def draw(weights, col, axes):
+    def line(a, b, x):
+        return a*x+b
+
+    def plane(d, a, b, c, x, y):
+        return -a*x/c-b*y/c+d/c
+    if weights.shape[0] == 4:
+        interval_x = np.arange (-10, 10, 1)
+        interval_y = np.arange (-10, 10, 1)
+        xgrid, ygrid = np.meshgrid(interval_x, interval_y)
+        zgrid = plane(weights[0], weights[1], weights[2], weights[3], xgrid, ygrid)
+        axes.plot_surface(xgrid, ygrid, zgrid, vmin = -10, vmax = 10, color = col, alpha=0.3)
+        axes.set_zlim(-10, 10)
+    if weights.shape[0] == 3:
+        interval = np.arange(-10, 10, 1)
+        pb.plot(interval, line(-weights[1]/weights[2], weights[0]/weights[2], interval))
+        pb.xlim([-10, 10])
+        pb.ylim([-10, 10])
 
 
-def grad_M(X, w, Xo):
-    # calculates gradient fo M
-    # returns two vectors - dMi/dXo and dMi/dw in X
-    return -1*X[0]*w, np.array(X[0]*(np.array(X[1:]) - Xo))
+def gradient_descent(function, initial):
+    def find_grad():
+        x = np.zeros(number)
+        x[:] = initial
 
+        for i in range(initial.size):
+            x[i] = initial[i] + h
+            grad[i] = (function(x) - function(initial))/h
+            x[i] = initial[i]
 
-def Q(w, Xo):
-    # calculates value for residual function
-    Q_value = 0.0
-    for X in points:
-        Q_value += np.log(1 + np.exp(-1*M(X, w, Xo)))
-    return Q_value
-
-def grad_Q(w, Xo):
-    # calculates gradient for residual function
-    # returns two vectors - dMi/dXo and dMi/dw in X
-    grad_Xo = np.zeros(2)
-    grad_w = np.zeros(2)
-    for X in points:
-        exp_buf = np.exp(-1*M(X, w, Xo))
-        grad_coeff = -1*exp_buf/(1 + exp_buf)
-        delta_grad_Xo, delat_grad_w = grad_M(X, w, Xo)
-        grad_Xo +=  grad_coeff*delta_grad_Xo
-        grad_w += grad_coeff*delat_grad_w
-    return grad_Xo, grad_w
-
-
-def make_step(Xo, w, grad_Xo, grad_w):
-    # calculates step via optimizing one-dimensional function F(t) = Q(Xo|w + t*gradQ(Xo, w))
-    # returns new Xo, w
-    min_step = acc/np.sqrt(norm(grad_Xo)**2 + norm(grad_w)**2)
-    t1 = 0
-    t2 = min_step
-    Q1, Q2 = Q(w, Xo), 0
-    while(True):
-        Q2 = Q(w - t2*grad_w, Xo - t2*grad_Xo)
-        if (Q2 < Q1):
-            Q1 = Q2
-            t1 = t2
-            t2 *= 2
-        else:
-             break
-    while True:
-        if t2-t1 <= min_step:
-            break
-        else:
-            t = (t1 + t2)/2.0
-            Q_mid = Q(w - t*grad_w, Xo - t*grad_Xo)
-            if(Q_mid < Q1):
-                t1 = t
-                Q1 = Q_mid
+    def choose_step(step):
+        status2 = 1
+        step1 = step
+        while status2==1:
+            if (function(initial - grad * step1) > (function(initial) - eps*step*math.sqrt(np.dot(grad,grad)))):
+                step1 *= delta
             else:
-                t2 = t
-                Q2 = Q_mid
-    if t1*np.sqrt(norm(grad_Xo)**2 + norm(grad_w)**2) < acc:
-        return Xo, w
-    else:
-        return Xo - t1*grad_Xo, w - t1*grad_w
+                status2 = 0
+        return step1
+    eps = 0.1
+    delta = 0.5
+    h = 0.0001
+    step = 0.01
+    number = initial.shape[0]
+    grad = np.zeros(number)
+    status = 1
+    while status != 0:
+        find_grad()
+        step = choose_step(step)
+        initial = initial - grad * step
+        if np.all(grad*step <= 0.001) and np.all(grad*step >= -0.001):
+            status = 0
+    norma = map(lambda x: x*x, initial)
+    norma = math.sqrt(reduce(lambda x, y: x+y, norma))
+    return initial/norma
 
 
-def draw_line(Xo, w, color):
-    # drawing dividing line by start point and normal vector
+def main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2):
+    fig = pb.figure()
+    data, axes = gen_data1(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2, fig)
+    l_regression = LogisticRegression()
+    l_regression.fit(data)
 
-    scale1 = 3
-    X_left = first_average_x - scale1*first_dispersion_x
-    X_right = second_average_x + scale1*second_dispersion_x
-    Y_top = first_average_y + scale1*first_dispersion_y
-    Y_bottom = second_average_y - scale1*second_dispersion_y
+    weights_by_grad = np.zeros(Expected1.shape[0]+1)
+    weights_by_grad = l_regression.find_weights(weights_by_grad)
 
-    if w[1] == 0:
-        direction = np.array([0, 1])
-    else:
-        direction = np.array([1, -1*w[0]/w[1]])
-        direction = direction/norm(direction)
-    if Xo[0] < X_left and direction[0] != 0:
-        Xo += direction*((X_left+X_right)/2 - Xo[0])/direction[0]
-    if Xo[0] > X_right and direction[0] != 0:
-        Xo -= direction*(Xo[0] - (X_left+X_right)/2)/direction[0]
+    weights_by_scipy = np.zeros(Expected1.shape[0]+1)
+    weights_by_scipy = minimize(l_regression.Q, weights_by_scipy, method='nelder-mead')
 
-    scale = 15
-    point1 = Xo + scale*direction
-    point2 = Xo - scale * direction
-    X = np.array([point1[0], point2[0]])
-    Y = np.array([point1[1], point2[1]])
-    lines = plt.plot(X, Y)
-    plt.setp(lines, color=color, linewidth=3.0)
+    norma2 = map(lambda x: x*x, weights_by_scipy.x)
+    norma2 = math.sqrt(reduce(lambda x, y: x+y, norma2))
+    weights_by_scipy.x /= norma2
+
+    print weights_by_grad, weights_by_scipy.x
+
+    draw(weights_by_grad, 'black', axes)
+    draw(weights_by_scipy.x, 'yellow', axes)
+    pb.show()
 
 
-# initialisation of start hyperplane
-# Xo - point in the hyperplane, W - normal vector to hyperplane
-# Xo = np.array([(first_average_x + second_average_x)/2.0, (first_average_y + second_average_y)/2.0])
-# w =  np.array([(second_average_x - first_average_x), (second_average_y - first_average_y)])
-Xo = np.array([10.0, -5.0])
-w = np.array([1.0, 0.0])
-draw_line(Xo, w, 'g')
+Expected1 = np.array([1, 4, 3])
+Expected2 = np.array([4, 3, 5])
+Dispersion1 = 1
+Dispersion2 = 1
+Number1 = 200
+Number2 = 200
 
-# gradient decent
-while(True):
-    grad_Xo, grad_w = grad_Q(w, Xo)
-    X1, w1 = make_step(Xo, w, grad_Xo, grad_w)
-    print Xo, w, np.sqrt(norm(X1-Xo)**2+ norm(w1 - w)**2)
-    if np.sqrt(norm(X1-Xo)**2+ norm(w1 - w)**2) <= acc:
-        break
-    else:
-        Xo, w = X1, w1
 
-# plotting final hyperplane
-print Xo, w
-draw_line(Xo, w, 'm')
-
-plt.show()
+main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2)
